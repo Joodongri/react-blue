a294103ceb0aa6505261c09ac9e608042c10db00
Darren Zhu <joodongri@gmail.com>
2019-10-31 17:33:41 -0700

    Debugged Electron on packaging

--- client/index.js
+++ client/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { MemoryRouter, Switch, Route } from 'react-router-dom';
-import LandingPage from './landing-page/LandingPage.jsx';
 import App from './App.jsx';
 import store from './store';
 import './assets/styles/normalize.css';
@@ -13,19 +11,9 @@ import './assets/styles/styles.css';
  * 
  * Wrapped Root Component in the Provider allowing children to be connected with Redux store
 **/
-
 render(
   <Provider store={store}>
-    <MemoryRouter>
-      <Switch>
-        <Route path='/' exact>
-          <LandingPage />
-        </Route>
-        <Route path='/application'>
-          <App />
-        </Route>
-      </Switch>
-    </MemoryRouter>
+    <App />
   </Provider>,
   document.getElementById('app')
 );
